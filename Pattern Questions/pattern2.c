90676f5ec2beacf2cc529f24774ec22c78877eb7
YubrajKarki <yubrajkarki528@gmail.com>
2023-02-25 21:03:18 +0545

    pattern2

---
+++ Pattern Questions/pattern2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    for(int row=1; row<=6; row++){
+        
+        for(int col=1; col<=4; col++){
+              printf("*"); 
+        }
+        
+        printf("\n");
+         
+    }
+
+
+    return 0;
+}         
