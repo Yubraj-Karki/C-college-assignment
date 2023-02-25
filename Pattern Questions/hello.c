6da8213f412314ba89b4de9e0d4eab35363d75f0
YubrajKarki <yubrajkarki528@gmail.com>
2023-02-25 20:44:57 +0545

    staircase pattern

--- Pattern Questions/hello.c
+++ Pattern Questions/hello.c
@@ -1,10 +1,16 @@
-#include <studio.h>
+#include <stdio.h>
 
 int main() {
+    for(int row=1; row<=4; row++){
+        
+        for(int col=1; col<=row; col++){
+              printf("*"); 
+        }
+        
+        printf("\n");
+         
+    }
 
-    printf("john");
+
     return 0;
-
-}
-
-
+}         
